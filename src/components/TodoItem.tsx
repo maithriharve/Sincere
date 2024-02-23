c5a44cf5ba4a22bef5f97d3108802cc8cc243fd7
Maithri Harve <maithri.harve@gmail.com>
2024-02-23 14:17:53 -0500

    Formatting

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Todo} from '@/lib/todos-lib';
-import {HiTrash} from 'react-icons/hi2';
-import {HiCheckCircle} from "react-icons/hi2";
+import { Todo } from '@/lib/todos-lib';
+import { HiTrash } from 'react-icons/hi2';
+import { HiCheckCircle } from "react-icons/hi2";
 
 interface TodoItemProps {
     todo: Todo;
@@ -10,7 +10,7 @@ interface TodoItemProps {
     Icon: React.ElementType;
 }
 
-export const TodoItem = ({todo, onTodoUpdate, onTodoDelete, Icon}: TodoItemProps) => {
+export const TodoItem = ({ todo, onTodoUpdate, onTodoDelete, Icon }: TodoItemProps) => {
     return (
         <div className="relative w-full" key={todo.id}>
             <button
@@ -18,7 +18,7 @@ export const TodoItem = ({todo, onTodoUpdate, onTodoDelete, Icon}: TodoItemProps
                 className="group flow-root w-full rounded border border-stone-200 bg-white px-4 py-3 text-xl transition-opacity text-left hover:bg-gray-100 focus-visible:outline-none disabled:opacity-50"
                 onClick={() => onTodoUpdate(todo, !todo.completed)}
             >
-                <Icon className={`text-xl inline ${Icon === HiCheckCircle ? 'fill-green-600' : ''}`} /> {todo.title} 
+                <Icon className={`text-xl inline ${Icon === HiCheckCircle ? 'fill-green-600' : ''}`} /> {todo.title}
                 <div className="float-right hidden group-hover:block" onClick={() => onTodoDelete(todo.id)}>
                     <HiTrash className="hover:fill-red-600 text-xl" />
                 </div>
